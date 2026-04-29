f31f25fdfa455838f40668e0fd458221bf87835d
MONU KUMAR YADAV <monukumaryadav4128@gmail.com>
2026-04-29 17:06:02 +0530

    Allow access to favicon.svg in SecurityConfig

--- src/main/java/in/scalive/Velora/config/SecurityConfig.java
+++ src/main/java/in/scalive/Velora/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                         "/",
                         "/index.html",
                         "/favicon.ico",
+                       "/favicon.svg",
                         "/fallback.png",
                         "/assets/**",
                         "/models/**"
