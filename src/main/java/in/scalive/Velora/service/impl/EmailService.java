68e37cad46fdd18d7a5bf1c685693ff523ebe0f6
MONU KUMAR YADAV <monukumaryadav4128@gmail.com>
2026-04-29 09:06:23 +0530

    Refactor EmailService to improve email formatting
    Updated email content formatting and added sender email.

--- src/main/java/in/scalive/Velora/service/impl/EmailService.java
+++ src/main/java/in/scalive/Velora/service/impl/EmailService.java
@@ -1,11 +1,5 @@
 package in.scalive.Velora.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
 public class EmailService {
 
     @Autowired
@@ -14,35 +8,40 @@ public class EmailService {
     public void sendOtp(String toEmail, String otp) {
 
         SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("your_email@gmail.com"); // 🔥 MUST ADD
         message.setTo(toEmail);
         message.setSubject("🎁 Velora - Verify Your Email");
 
         message.setText(
-            "Hi there! 👋" +
-            "Welcome to Velora – where every gift tells a story 🎀" +
-            "To complete your registration, please use the OTP below:" +
-            "🔐 OTP: " + otp +
-            " This OTP is valid for 5 minutes." +
+            "Hi there! 👋\n\n" +
+            "Welcome to Velora – where every gift tells a story 🎀\n\n" +
+            "To complete your registration, please use the OTP below:\n\n" +
+            "🔐 OTP: " + otp + "\n\n" +
+            "This OTP is valid for 5 minutes.\n\n" +
             "If you didn’t request this, you can safely ignore this email.\n\n" +
-            "With love 💖," +
+            "With love 💖,\n" +
             "Team Velora ✨"
         );
+
         mailSender.send(message);
-   }
-    
+    }
+
     public void sendWelcomeEmail(String toEmail, String name) {
 
         SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("your_email@gmail.com"); // 🔥 MUST ADD
         message.setTo(toEmail);
         message.setSubject("🎉 Welcome to Velora!");
 
         message.setText(
-            "Hi " + name + " 👋" +
-            "Welcome to Velora – where every gift tells a story 🎁✨" +
-            "Your account has been successfully created 🎉" +
-            "Now you can explore a world full of beautiful gifts and surprises 💝" +
-            "Start your journey with us and make every moment special!" +
-            "With love 💖," +
+            "Hi " + name + " 👋\n\n" +
+            "Welcome to Velora – where every gift tells a story 🎁✨\n\n" +
+            "Your account has been successfully created 🎉\n\n" +
+            "Now you can explore a world full of beautiful gifts and surprises 💝\n\n" +
+            "Start your journey with us and make every moment special!\n\n" +
+            "With love 💖,\n" +
             "Team Velora ✨"
         );
 
